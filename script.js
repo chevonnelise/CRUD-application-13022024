3642d220135a32b5b8fc5c7833b9853d19ef9999
chevonnelise <elise.chevonne@gmail.com>
2024-02-07 07:37:47 +0000

    add main function

--- script.js
+++ script.js
@@ -10,3 +10,36 @@ document.addEventListener("DOMContentLoaded", function(){
 
     main();
 });
+
+
+function renderTodos(todos) {
+    const todoList = document.querySelector('#todoList');
+    todoList.innerHTML = '';
+    for (let todo of todos) {
+        const li = document.createElement('li');
+        li.className = 'list-group-item d-flex justify-content-between align-items-center';
+        li.innerHTML = `
+                  ${todo.name} <span class="badge bg-primary">${todo.urgency}</span>               
+              `;
+        todoList.appendChild(li);
+    }
+}
+
+function main(){
+    let todos = [];
+
+    const addTodoButton = document.querySelector("#addTodo");
+    addTodoButton.addEventListener('click', function(){
+        const taskNameInput = document.querySelector("#taskName")
+        const taskName = taskNameInput.ariaValueMax;
+
+        const taskUrgencySelect = document.querySelector("#taskUrgency");
+        const taskUrgency = taskUrgencySelect.value;
+
+        if (taskName) {
+            addTodo(todos, taskName, taskUrgency);
+            renderTodos(todos);
+            taskNameInput.value = '';
+        }
+    });
+}

21c93e916c7d07d24362e4ed839e45d4714d9f1e
chevonnelise <elise.chevonne@gmail.com>
2024-02-07 07:14:28 +0000

    add if loop to check for existing index

--- data.js
+++ data.js
@@ -10,5 +10,20 @@ function addTodo(todos, name, urgency){
 }
 
 function modifyTask(todos, id, newName, newUrgency) {
-    
+    // creating the new task
+    let modifiedTask = {
+        "id": id,
+        "name": newName,
+        "urgency": newUrgency
+    }
+
+    // insert index of the task that user wants to replace
+    const indexToReplace = todos.findIndex(function(t){
+        return t.id == id;
+    });
+
+    // if loop to check if the findIndex can find the index, if not findIndex to return -1
+    if (indexToReplace > -1) {
+        todos[indexToReplace] = modifiedTask;
+    }
 }
